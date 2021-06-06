eefe9ba05ed0838404839c273661e6545e63122e
taro.yamada <kyoyu_pc@yahoo.co.jp>
2021-06-06 13:45:00 +0000

    user pages

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,7 +12,7 @@
             <ul class="navbar-nav">
                 @if (Auth::check())
                     {{-- ユーザ一覧ページへのリンク --}}
-                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item">{!! link_to_route('users.index', 'Users', [], ['class' => 'nav-link']) !!}</li>
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                         <ul class="dropdown-menu dropdown-menu-right">
@@ -29,6 +29,14 @@
                     {{-- ログインページへのリンク --}}
                     <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
                 @endif
+            </ul>
+            
+            <ul class="dropdown-menu dropdown-menu-right">
+                    {{-- ユーザ詳細ページへのリンク --}}
+                    <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
+                    <li class="dropdown-divider"></li>
+                    {{-- ログアウトへのリンク --}}
+                    <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
             </ul>
         </div>
     </nav>
