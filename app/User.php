d9ed496c00004e6f8cfd64b26f22d91db8afb4ea
taro.yamada <kyoyu_pc@yahoo.co.jp>
2021-06-07 08:10:29 +0000

    post

--- app/User.php
+++ app/User.php
@@ -37,5 +37,19 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     
+    /**
+     * このユーザが所有する投稿。（ Micropostモデルとの関係を定義）
+     */
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
     
+    /**
+     * このユーザに関係するモデルの件数をロードする。
+     */
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }
 }
